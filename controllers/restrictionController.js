77739dc83b90bf031e769845b662f40f07f4fd90
Jairo Calcina <jairo.calcina@outlook.com>
2024-09-29 20:48:54 -0300

    style: add prettier formatting

--- controllers/restrictionController.js
+++ controllers/restrictionController.js
@@ -1,75 +1,72 @@
 // controllers/restrictionController.js
-const db = require('../helpers/firebase');
-const Restriction = require('../models/restriction');
-const StudentRestriction = require('../models/studentRestriction');
-const { v4: uuidv4 } = require('uuid');
+const db = require("../helpers/firebase");
+const Restriction = require("../models/restriction");
+const StudentRestriction = require("../models/studentRestriction");
+const { v4: uuidv4 } = require("uuid");
 
 const getRestrictions = async (req, res, next) => {
-    const { uuid_student } = req.params;
+  const { uuid_student } = req.params;
 
-    try {
-      const studentRestrictionsSnapshot = await db
-        .collection('studentRestrictions')
-        .where('uuid_student', '==', uuid_student)
-        .get();
-  
-      if (studentRestrictionsSnapshot.empty) {
-        return res.status(200).json({
-          message: 'No restrictions found for this student.',
-          restrictions: [],
-        });
-      }
-  
-      let uuid_restrictions = [];
-      studentRestrictionsSnapshot.forEach((doc) => {
-        uuid_restrictions.push(doc.data().uuid_restriction);
+  try {
+    const studentRestrictionsSnapshot = await db
+      .collection("studentRestrictions")
+      .where("uuid_student", "==", uuid_student)
+      .get();
+
+    if (studentRestrictionsSnapshot.empty) {
+      return res.status(200).json({
+        message: "No restrictions found for this student.",
+        restrictions: [],
       });
-  
-      uuid_restrictions = [...new Set(uuid_restrictions)];
+    }
 
-      const restrictions = [];
-  
-      if (uuid_restrictions.length <= 10) {
-        const restrictionsSnapshot = await db
-          .collection('restrictions')
-          .where('uuid', 'in', uuid_restrictions)
-          .get();
-  
+    let uuid_restrictions = [];
+    studentRestrictionsSnapshot.forEach((doc) => {
+      uuid_restrictions.push(doc.data().uuid_restriction);
+    });
+
+    uuid_restrictions = [...new Set(uuid_restrictions)];
+
+    const restrictions = [];
+
+    if (uuid_restrictions.length <= 10) {
+      const restrictionsSnapshot = await db
+        .collection("restrictions")
+        .where("uuid", "in", uuid_restrictions)
+        .get();
+
+      restrictionsSnapshot.forEach((doc) => {
+        restrictions.push(doc.data());
+      });
+    } else {
+      const batches = [];
+      while (uuid_restrictions.length) {
+        const batch = uuid_restrictions.splice(0, 10);
+        batches.push(
+          db.collection("restrictions").where("uuid", "in", batch).get()
+        );
+      }
+
+      const results = await Promise.all(batches);
+      results.forEach((restrictionsSnapshot) => {
         restrictionsSnapshot.forEach((doc) => {
           restrictions.push(doc.data());
         });
-      } else {
-        const batches = [];
-        while (uuid_restrictions.length) {
-          const batch = uuid_restrictions.splice(0, 10);
-          batches.push(
-            db
-              .collection('restrictions')
-              .where('uuid', 'in', batch)
-              .get()
-          );
-        }
-  
-        const results = await Promise.all(batches);
-        results.forEach((restrictionsSnapshot) => {
-          restrictionsSnapshot.forEach((doc) => {
-            restrictions.push(doc.data());
-          });
-        });
-      }
-  
-      res.status(200).json({ restrictions });
-    } catch (error) {
-      next(error);
+      });
     }
+
+    res.status(200).json({ restrictions });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const validateStudent = async (req, res, next) => {
   const { uuid_student } = req.params;
   try {
     const snapshot = await db
-      .collection('studentRestrictions')
-      .where('uuid_student', '==', uuid_student)
+      .collection("studentRestrictions")
+      .where("uuid_student", "==", uuid_student)
       .get();
 
     if (snapshot.empty) {
@@ -88,41 +85,41 @@ const validateStudent = async (req, res, next) => {
 };
 
 const assignRestriction = async (req, res, next) => {
-    const { uuid_student, reason } = req.body;
+  const { uuid_student, reason } = req.body;
 
-    try {
-      const uuid_restriction = uuidv4();
-  
-      await db.collection('restrictions').doc(uuid_restriction).set({
-        uuid: uuid_restriction,
-        reason,
-      });
-  
-      await db.collection('studentRestrictions').add({
-        uuid_student,
-        uuid_restriction,
-      });
-  
-      res.status(201).json({
-        message: 'Restriction created and assigned successfully.',
-        uuid_restriction,
-      });
-    } catch (error) {
-      next(error);
-    }
+  try {
+    const uuid_restriction = uuidv4();
+
+    await db.collection("restrictions").doc(uuid_restriction).set({
+      uuid: uuid_restriction,
+      reason,
+    });
+
+    await db.collection("studentRestrictions").add({
+      uuid_student,
+      uuid_restriction,
+    });
+
+    res.status(201).json({
+      message: "Restriction created and assigned successfully.",
+      uuid_restriction,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const removeRestriction = async (req, res, next) => {
   const { uuid_student, uuid_restriction } = req.body;
   try {
     const snapshot = await db
-      .collection('studentRestrictions')
-      .where('uuid_student', '==', uuid_student)
-      .where('uuid_restriction', '==', uuid_restriction)
+      .collection("studentRestrictions")
+      .where("uuid_student", "==", uuid_student)
+      .where("uuid_restriction", "==", uuid_restriction)
       .get();
 
     if (snapshot.empty) {
-      return res.status(404).json({ message: 'Restriction not found.' });
+      return res.status(404).json({ message: "Restriction not found." });
     }
 
     const batch = db.batch();
@@ -131,15 +128,15 @@ const removeRestriction = async (req, res, next) => {
     });
     await batch.commit();
 
-    res.status(200).json({ message: 'Restriction removed successfully.' });
+    res.status(200).json({ message: "Restriction removed successfully." });
   } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
-    getRestrictions,
-    validateStudent,
-    assignRestriction,
-    removeRestriction,
+  getRestrictions,
+  validateStudent,
+  assignRestriction,
+  removeRestriction,
 };
