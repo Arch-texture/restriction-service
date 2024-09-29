446752bcd9d9d5b158a1f56516b7948932ac76fc
Jairo Calcina <jairo.calcina@outlook.com>
2024-09-29 17:10:18 -0300

    feat: add studentRestriction model

---
+++ models/studentRestriction.js
@@ -0,0 +1,8 @@
+class StudentRestriction {
+    constructor(uuid_student, uuid_restriction) {
+        this.uuid_student = uuid_student;
+        this.uuid_restriction = uuid_restriction;
+    }
+}
+
+module.exports = StudentRestriction;
