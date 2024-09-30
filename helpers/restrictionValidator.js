dddf06dc439683259081857580ad75f07195ea4f
Jairo Calcina <jairo.calcina@outlook.com>
2024-09-29 22:36:53 -0300

    feat: add validators for restrictions

--- helpers/restrictionValidator.js
+++ helpers/restrictionValidator.js
@@ -1,5 +1,14 @@
 const { body, param } = require("express-validator");
 
+exports.createRestrictionValidation = [
+  body("reason")
+    .trim()
+    .notEmpty()
+    .withMessage("Reason is required.")
+    .isLength({ max: 255 })
+    .withMessage("Reason cannot exceed 255 characters."),
+];
+
 exports.assignRestrictionValidation = [
   body("uuid_student")
     .isUUID()
@@ -16,6 +25,12 @@ exports.validateStudentValidation = [
   param("uuid_student")
     .isUUID()
     .withMessage("uuid_student must be a valid UUID."),
+];
+
+exports.validateRestrictionValidation = [
+  param("uuid_restriction")
+    .isUUID()
+    .withMessage("uuid_restriction must be a valid UUID."),
 ];
 
 exports.removeRestrictionValidation = [
