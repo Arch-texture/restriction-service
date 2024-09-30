243ea54ba2b04f951c0ea85ce23c2d0210adc8f2
Jairo Calcina <jairo.calcina@outlook.com>
2024-09-29 22:35:00 -0300

    feat: add functions to create and validate a restriction

--- controllers/restrictionController.js
+++ controllers/restrictionController.js
@@ -1,8 +1,25 @@
-// controllers/restrictionController.js
 const db = require("../helpers/firebase");
 const Restriction = require("../models/restriction");
 const StudentRestriction = require("../models/studentRestriction");
 const { v4: uuidv4 } = require("uuid");
+
+
+const createRestriction = async (req, res, next) => {
+  const { reason } = req.body;
+
+  try {
+    const uuid_restriction = uuidv4();
+
+    await db.collection("restrictions").doc(uuid_restriction).set({
+      uuid: uuid_restriction,
+      reason,
+    });
+
+    res.status(201).json({ message: "Restriction created successfully.", uuid_restriction });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getRestrictions = async (req, res, next) => {
   const { uuid_student } = req.params;
@@ -73,16 +90,29 @@ const validateStudent = async (req, res, next) => {
       return res.status(200).json({ restricted: false });
     }
 
-    const restrictions = [];
-    snapshot.forEach((doc) => {
-      restrictions.push(doc.data().uuid_restriction);
-    });
-
-    res.status(200).json({ restricted: true, restrictions });
+    res.status(200).json({ restricted: true });
   } catch (error) {
     next(error);
   }
 };
+
+const validateRestriction = async (req, res, next) => {
+  const { uuid_restriction } = req.params;
+  try {
+    const snapshot = await db
+      .collection("restrictions")
+      .where("uuid", "==", uuid_restriction)
+      .get();
+
+    if (snapshot.empty) {
+      return res.status(404).json({ message: "Restriction not found." });
+    }
+
+    res.status(200).json({ message: "Restriction found." });
+  } catch (error) {
+    next(error);
+  }
+}
 
 const assignRestriction = async (req, res, next) => {
   const { uuid_student, reason } = req.body;
@@ -102,7 +132,9 @@ const assignRestriction = async (req, res, next) => {
 
     res.status(201).json({
       message: "Restriction created and assigned successfully.",
+      uuid_student,
       uuid_restriction,
+      reason,
     });
   } catch (error) {
     next(error);
@@ -135,8 +167,10 @@ const removeRestriction = async (req, res, next) => {
 };
 
 module.exports = {
+  createRestriction,
   getRestrictions,
   validateStudent,
+  validateRestriction,
   assignRestriction,
   removeRestriction,
 };
